800212ea14b801bdb7b1880b3b8198a26fa12a06
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-10-04 16:56:01 +0600

    Update Clock Conversion.cpp

--- CodeForce/Clock Conversion.cpp
+++ CodeForce/Clock Conversion.cpp
@@ -1,1 +1,55 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string convertTo12Hour(int hour, int minute) {
+    if (hour < 0 || hour > 23 || minute < 0 || minute > 59) {
+        return "Invalid input";
+    }
+
+    string period = "AM";
+
+    if (hour >= 12) {
+        period = "PM";
+        if (hour > 12) {
+            hour -= 12;
+        }
+    }
+    
+    if (hour == 0) {
+        hour = 12;
+    }
+
+
+    string minuteStr = to_string(minute);
+    if (minute < 10) {
+        minuteStr = "0" + minuteStr;
+    }
+   string hours=to_string(hour);
+   if(hour<10){
+       hours="0"+hours;
+   }
+    return  hours+ ":" + minuteStr + " " + period;
+}
+
+int main() {
+    int test;
+    cin >> test;
+    cin.ignore();
+    
+    while (test--) {
+        string input;
+        getline(cin, input);
+        int hour, minute;
+        stringstream ss(input);
+        char colon;
+        if (ss >> hour >> colon >> minute && colon == ':') {
+            cout << convertTo12Hour(hour, minute) << endl;
+        }
+    }
+
+    return 0;
+}
 

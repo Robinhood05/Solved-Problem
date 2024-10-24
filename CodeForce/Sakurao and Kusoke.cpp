68a27c8cc0c39d428e7c12f27bb049780c06f58f
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-10-24 22:53:11 +0600

    Create Sakurao and Kusoke.cpp

---
+++ CodeForce/Sakurao and Kusoke.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{ 
+    int t;
+    cin>>t;
+    while(t--){
+        int number;
+        cin>>number;
+        if(number%2==0){
+            cout<<"Sakurako"<<endl;
+        }else{
+            cout<<"Kosuke"<<endl;
+        }
+    }
+}

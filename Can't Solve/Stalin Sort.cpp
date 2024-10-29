85c61866619ece84718f09731b215834946fb26c
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-10-29 23:05:31 +0600

    Solve Stalin Sort

--- Can't Solve/Stalin Sort.cpp
+++ Can't Solve/Stalin Sort.cpp
@@ -1,66 +1,41 @@
-#include <iostream>
-#include <bits/stdc++.h>
 
+
+#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-int counts = 0;
-unordered_set<long long int> s;
 
-bool check_sort(vector<long long int> v) {
-    for (size_t i = 1; i < v.size(); i++) {
-        if (v[i] > v[i - 1]) {
-            return false;
-        }
-    }
-    return true;
-}
-
-int stalin_sort(vector<long long int> v) {
-    counts++;
-    vector<long long int> new_vector;
-
-    if (!v.empty()) {
-        new_vector.push_back(v[0]);
-    }
-
-    for (size_t i = 1; i < v.size(); i++) {
-        if (v[i] < new_vector.back()) {
-            new_vector.push_back(v[i]);
-        } else {
-            s.insert(v[i]);
-           // cout << v[i] << " will be removed" << endl;
-        }
-    }
-
-    if (check_sort(new_vector)) {
-        return counts;
-    } else {
-        //cout << "Stalin called one time" << endl;
-        return stalin_sort(new_vector); 
-    }
-}
-
-int main() {
+int main()
+{
+    std::cout<<"Hello World";
+    
     int test;
-    cin >> test;
-    while (test--) {
-        int size;
-        cin >> size;
-
-        vector<long long int> v(size);
-        for (int i = 0; i < size; i++) {
-            cin >> v[i];
+    cin>>test;
+    
+    while(test--){
+        int n;
+        cin>>n;
+        vector<int>v;
+        
+        for(int i = 0 ;i<n;i++){
+            int x;
+            cin>>x;
+            v.push_back(x);
         }
-
-        if (check_sort(v)) {
-            cout << 0 << endl;
-        } else {
-            int ans = stalin_sort(v);
-            cout << ans+1 << endl;
+        int remv = n;
+        for(int i = 0 ;i<n;i++){
+            int count = i;
+            
+            for(int j = i+1;j<n;j++){
+                if(v[i]<v[j]){
+                    count++;
+                }
+            }
+            remv = min(remv,count);
+            
         }
+        cout<<remv<<endl;
     }
 
     return 0;
 }
-
-//link: https://codeforces.com/contest/2027/problem/B

196f56d1fb83c80898fdda9b7302060d985d8bf6
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-10-30 22:43:55 +0600

    D - Kyoya and Photobooks String

---
+++ UVA/String add.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    //std::cout<<"Hello World";
+    
+    string s;
+    cin>>s;
+    
+    int size = s.length();
+    
+    int total = (size+1) *25;
+    cout<<total+1<<endl;
+    
+   // cout<<(size+1) *26<<endl;
+    
+
+    return 0;
+}

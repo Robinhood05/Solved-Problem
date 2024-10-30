6e72b91903b432c11f8f90b5a1fb19de1edb3eff
Robin <99762946+Robinhood05@users.noreply.github.com>
2024-10-30 22:45:18 +0600

    Create Again 25.cpp

---
+++ UVA/Again 25.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//#define MOD 1000000007
+
+int main() {
+    int n;
+    //answer = 1;
+    cin >> n;
+   /* for (int i = 0; i < n; i++) {
+        answer *= 5;
+        answer %= MOD;
+    }
+    */
+    
+    if(n>=2){
+        cout<<"25"<<endl;
+    }
+    else {
+        cout<<"-1"<<endl;
+    }
+    //cout << answer<<endl;
+    return 0;
+}
